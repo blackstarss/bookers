32fea19348e1d2ac02dd09c0a02dbf77d240ffc8
blackstarss <kota.weeds1204@gmail.com>
2021-10-09 14:02:46 +0000

    #1復習と改善

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+    
+  end
+
+  def show
+  end
+
+  def top
+  end
+
+  def edit
+  end
+  
+  pri
+  
+  
+end
