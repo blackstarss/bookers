25326955b59a021d1abbe2ae06bf5e06bb39e26e
blackstarss <kota.weeds1204@gmail.com>
2021-10-10 07:57:54 +0000

    [#2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,33 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def top
-  end
+
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  pri
+  def create
+    book = Book.create(book_params)
+    
+    redirect_to book_path(book)
+    
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  
+  private
+   def book_params
+     params.require(:book).permit(:title, :body)
+   end
   
   
 end
